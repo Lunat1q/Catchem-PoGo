48f46af4c6531611799510d4c48dfb3be0d0295f
Bay101 <baylin.ferguson@gmail.com>
2016-08-29 13:08:42 +1000

    updates from lunat1q

--- Catchem/Pages/RouteCreatorPage.xaml.cs
+++ Catchem/Pages/RouteCreatorPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,17 +20,66 @@ using Catchem.Classes;
 using Catchem.Extensions;
 using GMap.NET;
 using GMap.NET.WindowsPresentation;
+=======
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Catchem.Classes;
+using Catchem.Extensions;
+using GeoCoordinatePortable;
+using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using PoGo.PokeMobBot.Logic;
+using PoGo.PokeMobBot.Logic.API;
+using PoGo.PokeMobBot.Logic.PoGoUtils;
+>>>>>>> refs/remotes/Lunat1q/dev
 
 namespace Catchem.Pages
 {
     /// <summary>
     /// Interaction logic for RouteCreatorPage.xaml
     /// </summary>
+<<<<<<< HEAD
 
 
 
     public partial class RouteCreatorPage : UserControl
     {
+=======
+    public partial class RouteCreatorPage
+    {
+        private CatchemSettings _globalSettings;
+
+        private readonly ObservableCollection<RouteMarker> _mapPoints = new ObservableCollection<RouteMarker>();
+        private GMapRoute _currentRoute;
+        private bool _builded;
+        private List<GeoCoordinate> _buildedRoute;
+        private bool _prefferMapzen;
+
+        public void SetGlobalSettings(CatchemSettings settings)
+        {
+            _globalSettings = settings;
+            RouteCreatorMap.MapProvider = _globalSettings.Provider;
+            _globalSettings.BindNewMapProbider((provider) =>
+            {
+                RouteCreatorMap.MapProvider = provider;
+                return true;
+            });
+            RoutesListBox.ItemsSource = _globalSettings.Routes;
+            var bot = MainWindow.BotsCollection.FirstOrDefault();
+            if (bot != null)
+            {
+                RouteCreatorMap.Position = new PointLatLng(bot.GlobalSettings.LocationSettings.DefaultLatitude,
+                    bot.GlobalSettings.LocationSettings.DefaultLongitude);
+            }
+        }
+>>>>>>> refs/remotes/Lunat1q/dev
 
         public RouteCreatorPage()
         {
@@ -57,6 +107,7 @@ namespace Catchem.Pages
             await Task.Delay(10);
         }
 
+<<<<<<< HEAD
         private void btn_GenerateRoute_Click(object sender, EventArgs e)
         {
             
@@ -121,3 +172,274 @@ namespace Catchem.Pages
         }
     }
 }
+=======
+        private void RouteCreatorMap_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            SlMapZoom.Value = RouteCreatorMap.Zoom;
+        }
+        private void sl_mapZoom_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var sl = (sender as Slider);
+            if (sl == null) return;
+            RouteCreatorMap.Zoom = (int)sl.Value;
+        }
+
+        internal void AddMarker(RouteMarker marker, bool starter)
+        {
+            if (starter)
+            {
+                try
+                {
+                    while (_mapPoints.Any(x => x.IsStart))
+                    {
+                        var mark = _mapPoints.First(x => x.IsStart);
+                        RemoveMarker(mark);
+                    }
+                }
+                catch (Exception)
+                {
+                    //ignore
+                }
+            }
+            _mapPoints.Add(marker);
+           RouteCreatorMap.Markers.Add(marker.Marker);
+            UpdateMarkerCounter();
+        }
+
+        private void UpdateMarkerCounter()
+        {
+            PointsNumber.Content = $"Number of waypoints: {_mapPoints.Count}";
+        }
+
+        private void RemoveMarker(RouteMarker rm)
+        {
+            RouteCreatorMap.Markers.Remove(rm.Marker);
+            rm.Marker.Clear();
+            _mapPoints.Remove(rm);
+            UpdateMarkerCounter();
+        }
+
+        private void MiSetStart_Click(object sender, RoutedEventArgs e)
+        {
+            var mi = sender as MenuItem;
+            var cm = mi?.Parent as ContextMenu;
+            var point = cm?.Tag as Point?;
+            if (point == null) return;
+            CreateNewMarker((Point)point, true);
+        }
+
+        private void MiSetWp_Click(object sender, RoutedEventArgs e)
+        {
+            var mi = sender as MenuItem;
+            var cm = mi?.Parent as ContextMenu;
+            var point = cm?.Tag as Point?;
+            if (point == null) return;
+            CreateNewMarker((Point)point, false);
+        }
+
+        private void CreateNewMarker(Point point, bool starter)
+        {
+            _builded = false;
+            var rPoint = point;
+            var mapPos = RouteCreatorMap.FromLocalToLatLng((int) rPoint.X, (int) rPoint.Y);
+            CreateNewMarker(mapPos, starter);
+        }
+
+        private void CreateNewMarker(PointLatLng mapPos, bool starter)
+        {
+            var markerShape = starter ? Properties.Resources.force_move.ToImage("Route Marker - START") :
+               Properties.Resources.wp.ToImage("Route Marker - Waypoint");
+            var marker = new GMapMarker(mapPos)
+            {
+                Shape = markerShape,
+                Offset = starter ? new Point(-24, -48) : new Point(-16, -32),
+                ZIndex = int.MaxValue,
+                Position = mapPos
+            };
+            var rm = new RouteMarker
+            {
+                IsStart = starter,
+                Location = new GeoCoordinate(mapPos.Lat, mapPos.Lng),
+                Marker = marker
+            };
+            markerShape.MouseLeftButtonDown += delegate { RemoveMarker(rm); };
+            AddMarker(rm, starter);
+        }
+
+
+
+        internal class RouteMarker
+        {
+            public GMapMarker Marker;
+            public GeoCoordinate Location;
+            public bool IsStart;
+        }
+
+        private void RouteCreatorMap_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            // Get cursor position in screen coordinates
+            var screenPoint = Mouse.GetPosition(RouteCreatorMap);
+
+            RouteCm.Tag = screenPoint;
+        }
+
+        private string GetWorkingRouting(out BotWindowData bot)
+        {
+            var botGoogle =
+                MainWindow.BotsCollection.FirstOrDefault(
+                    x => !string.IsNullOrEmpty(x.GlobalSettings.LocationSettings.GoogleDirectionsApiKey));
+
+            var botMapzen =
+                MainWindow.BotsCollection.FirstOrDefault(
+                    x => !string.IsNullOrEmpty(x.GlobalSettings.LocationSettings.MapzenValhallaApiKey));
+
+            bot = _prefferMapzen
+                ? (botMapzen ?? botGoogle)
+                : (botGoogle ?? botMapzen);
+
+            if (botGoogle == null && botMapzen == null) return "error";
+
+            return _prefferMapzen
+                ? (botMapzen != null ? "mapzen" : "google")
+                : (botGoogle != null ? "google" : "mapzen");
+        }
+
+        private async void BuildTheRoute_Click(object sender, RoutedEventArgs e)
+        {
+            if (_mapPoints.Count < 2) return;
+            if (_mapPoints.Count > 20)
+            {
+                _prefferMapzen = true;
+                PrefferMapzenOverGoogleCb.IsChecked = true;
+            }
+            if (_mapPoints.Count > 47)
+            {
+                MessageBox.Show(
+                    "Too many waypoints, try to reduce them to 45, or wait for next releases, where that limit will be increased!",
+                    "Routing Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+                BotWindowData bot;
+            var route = GetWorkingRouting(out bot);
+            if (route == "error")
+            {
+                MessageBox.Show(
+                    "You have to enter Google Direction API or Mapzen Valhalla API to any of your bots, before creating a route",
+                    "API Key Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var start = _mapPoints.FirstOrDefault(x => x.IsStart) ?? _mapPoints.First();
+
+            RoutingResponse response = null;
+            var cycleWp = _mapPoints.Where(x => !x.IsStart).Select(x => x.Location).ToList();
+            cycleWp.Add(start.Location);
+            if (route == "google")
+            {
+                response = GoogleRouting.GetRoute(start.Location, null, bot.Session, cycleWp, true);
+            }
+            else if (route == "mapzen")
+            {
+                response = MapzenRouting.GetRoute(start.Location, null, bot.Session, cycleWp, true);
+            }
+            if (response == null || response.Coordinates.Count == 0) return;
+
+            _currentRoute?.Points?.Clear();
+            if (_currentRoute == null)
+            {
+                _currentRoute = new GMapRoute(new List<PointLatLng>());
+                RouteCreatorMap.Markers.Add(_currentRoute);
+            }
+
+            var routePoints = response.Coordinates.Select(wp => new GeoCoordinate(wp[0], wp[1])).ToList();
+
+            _buildedRoute = new List<GeoCoordinate>(routePoints);
+
+            foreach (var item in routePoints)
+            {
+                _currentRoute.Points?.Add(new PointLatLng(item.Latitude, item.Longitude));
+            }
+            
+            _currentRoute?.RegenerateShape(RouteCreatorMap);
+            var path = _currentRoute?.Shape as Path;
+            if (path != null)
+                path.Stroke = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+
+
+            bot = MainWindow.BotsCollection.FirstOrDefault(
+                       x => !string.IsNullOrEmpty(x.GlobalSettings.LocationSettings.MapzenApiElevationKey));
+            if (bot != null)
+            {
+               await bot.Session.MapzenApi.FillAltitude(_buildedRoute.ToList());
+            }
+            _builded = true;
+        }
+
+        private void ClearTheRoute_Click(object sender, RoutedEventArgs e)
+        {
+            ClearRouteBuilder();
+        }
+
+        private void ClearRouteBuilder()
+        {
+            if (_currentRoute != null)
+            {
+                RouteCreatorMap.Markers.Remove(_currentRoute);
+            }
+            _builded = false;
+            while (_mapPoints.Any())
+            {
+                RemoveMarker(_mapPoints[0]);
+            }
+        }
+
+        private void SaveTheRoute_Click(object sender, RoutedEventArgs e)
+        {
+            var routeName = NewRouteNameBox.Text;
+            if (string.IsNullOrEmpty(routeName)) return;
+            if (!_builded)
+            {
+                MessageBox.Show(
+                    "Build the route first",
+                    "Routing error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if (_globalSettings.Routes.Any(x => string.Equals(x.Name, routeName, StringComparison.CurrentCultureIgnoreCase)))
+                routeName += "_" + DeviceSettings.RandomString(5);
+            var route = new BotRoute
+            {
+                Name = routeName,
+                InitialWp = _mapPoints.Select(x => x.Location).ToList(),
+                Route = new CustomRoute()
+                {
+                    RoutePoints = new List<GeoCoordinate>(_buildedRoute)
+                }
+            };
+            _globalSettings.Routes.Add(route);
+            NewRouteNameBox.Text = string.Empty;
+        }
+
+        private void RoutesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var lb = sender as ListBox;
+            if (lb == null || lb.SelectedIndex < 0) return;
+            var selectedRoute = lb.SelectedItem as BotRoute;
+            if (selectedRoute == null) return;
+            ClearRouteBuilder();
+            var start = true;
+            foreach (var wp in selectedRoute.InitialWp)
+            {
+                CreateNewMarker(new PointLatLng(wp.Latitude, wp.Longitude), start);
+                if (start) start = false;
+            }
+        }
+
+        private void PrefferMapzenOverGoogleCb_Checked(object sender, RoutedEventArgs e)
+        {
+            var cb = sender as CheckBox;
+            if (cb?.IsChecked == null) return;
+            _prefferMapzen = (bool)cb.IsChecked;
+        }
+    }
+}
+>>>>>>> refs/remotes/Lunat1q/dev
--- Catchem/Pages/TelegramPage.xaml.cs
+++ Catchem/Pages/TelegramPage.xaml.cs
@@ -51,18 +51,20 @@ namespace Catchem.Pages
             TelegramLogWorker();
             TelegramCommandWorker();
         }
+
         public void TurnOff()
         {
             _windowClosing = true;
             TlgrmBot?.Stop();
         }
+
         #region UI handlers
 
         public void SaveSettings()
         {
             var settingsPath = Path.Combine(Directory.GetCurrentDirectory(), TlgrmFilePath);
             var jsonSettings = new JsonSerializerSettings();
-            jsonSettings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
+            jsonSettings.Converters.Add(new StringEnumConverter {CamelCaseText = true});
             jsonSettings.ObjectCreationHandling = ObjectCreationHandling.Replace;
             jsonSettings.DefaultValueHandling = DefaultValueHandling.Populate;
 
@@ -123,11 +125,12 @@ namespace Catchem.Pages
             if (File.Exists(settingsPath))
             {
                 var jsonSettings = new JsonSerializerSettings();
-                jsonSettings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
+                jsonSettings.Converters.Add(new StringEnumConverter {CamelCaseText = true});
                 jsonSettings.ObjectCreationHandling = ObjectCreationHandling.Replace;
                 jsonSettings.DefaultValueHandling = DefaultValueHandling.Populate;
 
-                _tlgrmSettings = SerializeUtils.DeserializeDataJson<TelegramSettings>(settingsPath) ?? new TelegramSettings();
+                _tlgrmSettings = SerializeUtils.DeserializeDataJson<TelegramSettings>(settingsPath) ??
+                                 new TelegramSettings();
                 return;
             }
             _tlgrmSettings = new TelegramSettings();
@@ -137,7 +140,7 @@ namespace Catchem.Pages
         {
 
             if (AddToAutoReport.SelectedIndex <= -1) return;
-            var pokemonId = (PokemonId)AddToAutoReport.SelectedItem;
+            var pokemonId = (PokemonId) AddToAutoReport.SelectedItem;
             if (!_tlgrmSettings.AutoReportPokemon.Contains(pokemonId))
                 _tlgrmSettings.AutoReportPokemon.Add(pokemonId);
             AddToAutoReport.SelectedIndex = -1;
@@ -169,7 +172,7 @@ namespace Catchem.Pages
 
         public void TelegramCommandReceiver(string sender, string command, long chatId, string[] args)
         {
-            if (!CheckOwner(sender)) return;
+            if (!RefreshOwnerChatId(sender, chatId)) return;
 
             var cmd = new TelegramCommand
             {
@@ -188,11 +191,14 @@ namespace Catchem.Pages
             _logQueue.Enqueue(message);
         }
 
-        private void PokemonCaught(PokemonId pokemon, int cp, double iv, string profileName, string botNick, double level, PokemonMove? move1, PokemonMove? move2)
+        private void PokemonCaught(PokemonId pokemon, int cp, double iv, string profileName, string botNick,
+            double level, PokemonMove? move1, PokemonMove? move2)
         {
             if ((!_tlgrmSettings.AutoReportSelectedPokemon || !_tlgrmSettings.AutoReportPokemon.Contains(pokemon)) &&
-                (cp <= _tlgrmSettings.ReportAllPokemonsAboveCp || _tlgrmSettings.ReportAllPokemonsAboveCp <= 0)) return;
-            string messageToSend = $"[{botNick}]({profileName}) got {pokemon}! CP:{cp}, Iv:{iv.ToN1()}, Level:{level.ToN1()}, Move 1: {move1}, Move 2: {move2}";
+                (cp <= _tlgrmSettings.ReportAllPokemonsAboveCp || _tlgrmSettings.ReportAllPokemonsAboveCp <= 0))
+                return;
+            string messageToSend =
+                $"[{botNick}]({profileName}) got {pokemon}! CP:{cp}, Iv:{iv.ToN1()}, Level:{level.ToN1()}, Move 1: {move1}, Move 2: {move2}";
 
             foreach (var owner in _tlgrmSettings.Owners.Where(x => x.ChatId != 0))
             {
@@ -211,7 +217,6 @@ namespace Catchem.Pages
                     {
                         case "/start":
                             HandleHelp(t.ChatId);
-                            RefreshOwnerChatId(t.Sender, t.ChatId);
                             break;
                         case "help":
                             HandleHelp(t.ChatId);
@@ -235,7 +240,11 @@ namespace Catchem.Pages
                             HandleReport(t.ChatId, t.Args);
                             break;
                         case "reportabovecp":
+<<<<<<< HEAD
                             HandleReportAboveCP(t.ChatId, t.Args);
+=======
+                            HandleReportAboveCp(t.ChatId, t.Args);
+>>>>>>> refs/remotes/Lunat1q/dev
                             break;
                         default:
                             HandleUnknownCommand(t.ChatId);
@@ -246,16 +255,12 @@ namespace Catchem.Pages
             }
         }
 
-        private bool CheckOwner(string senderName)
-        {
-            return _tlgrmSettings.Owners.Any(x => x.TelegramName == senderName);
-        }
-
-        private void RefreshOwnerChatId(string senderName, long chatId)
+        private bool RefreshOwnerChatId(string senderName, long chatId)
         {
             var targetOwner = _tlgrmSettings?.Owners?.FirstOrDefault(x => x.TelegramName == senderName);
-            if (targetOwner == null) return;
+            if (targetOwner == null) return false;
             targetOwner.ChatId = chatId;
+            return true;
         }
 
         private void HandleUnknownCommand(long chatId)
@@ -293,18 +298,18 @@ namespace Catchem.Pages
             //var rank = 1;
             var topPokemon = new StringBuilder();
             IEnumerable<string> sb = null;
-            if (args[1].ToLower() == "cp") 
+            if (args[1].ToLower() == "cp")
             {
                 topPokemon.AppendLine($"Top 10 Highest CP Poke for {targetBot.ProfileName}: \n");
                 sb = targetBot.PokemonList?.OrderByDescending(x => x.Cp).Take(10)
                     .Select((x, i) => BuildPokemonRow(i + 1, x));
-               
+
             }
             if (args[1].ToLower() == "iv")
             {
                 topPokemon.AppendLine($"Top 10 Highest IV Poke for {targetBot.ProfileName}: \n");
                 sb = targetBot.PokemonList?.OrderByDescending(x => x.Iv).Take(10)
-                   .Select((x, i) => BuildPokemonRow(i + 1, x));
+                    .Select((x, i) => BuildPokemonRow(i + 1, x));
             }
             if (sb != null)
                 foreach (var s in sb)
@@ -313,7 +318,8 @@ namespace Catchem.Pages
             TlgrmBot.SendToTelegram(topPokemon.ToString(), chatId);
         }
 
-        private static string BuildPokemonRow(int indx, PokemonUiData pokemon) => $"{indx}) {pokemon.Name} CP:{pokemon.Cp} IV:{pokemon.Iv.ToN1()}";
+        private static string BuildPokemonRow(int indx, PokemonUiData pokemon)
+            => $"{indx}) {pokemon.Name} CP:{pokemon.Cp} IV:{pokemon.Iv.ToN1()}";
 
         private void HandleStatus(long chatId, string[] args)
         {
@@ -343,9 +349,12 @@ namespace Catchem.Pages
                     status.AppendLine($"Current Status for {targetBot.ProfileName}:");
                     status.AppendLine($"Current bot Run Time: {targetBot.Ts}");
                     status.AppendLine($"Level: {targetBot.Level} Exp/h: {targetBot.Xpph.ToN1()}");
-                    status.AppendLine($"Stardust {targetBot.StarDust}, Farmed: {(targetBot.StarDust > 0 ? targetBot.StarDust - targetBot.StartStarDust : 0)} ({targetBot.StardustRate.ToN1()}/h)");
-                    status.AppendLine($"Poke Caught: {targetBot.Stats?.TotalPokemons} ({targetBot.PokemonsRate.ToN1()}/h)");
-                    status.AppendLine($"PokeStops spinned: {targetBot.Stats?.TotalPokestops} ({targetBot.PokestopsRate.ToN1()}/h)");
+                    status.AppendLine(
+                        $"Stardust {targetBot.StarDust}, Farmed: {(targetBot.StarDust > 0 ? targetBot.StarDust - targetBot.StartStarDust : 0)} ({targetBot.StardustRate.ToN1()}/h)");
+                    status.AppendLine(
+                        $"Poke Caught: {targetBot.Stats?.TotalPokemons} ({targetBot.PokemonsRate.ToN1()}/h)");
+                    status.AppendLine(
+                        $"PokeStops spinned: {targetBot.Stats?.TotalPokestops} ({targetBot.PokestopsRate.ToN1()}/h)");
                     // Status.AppendLine($"Team: {targetBot.Stats.}");
                     TlgrmBot.SendToTelegram(status.ToString(), chatId);
                     return;
@@ -427,7 +436,8 @@ namespace Catchem.Pages
             botStringBuilder.AppendLine("Current Bots Avaliable:");
             foreach (var bot in MainWindow.BotsCollection)
             {
-                botStringBuilder.AppendLine($"{++botNumber}) {bot.ProfileName} [{(bot.Started ? "RUNNING" : "STOPPED")}]");
+                botStringBuilder.AppendLine(
+                    $"{++botNumber}) {bot.ProfileName} [{(bot.Started ? "RUNNING" : "STOPPED")}]");
             }
             if (botNumber == 0)
                 TlgrmBot.SendToTelegram("There are no bots created", chatId);
@@ -469,21 +479,35 @@ namespace Catchem.Pages
             TlgrmBot.SendToTelegram("Error Invalid Command Structure!", chatId);
         }
 
+<<<<<<< HEAD
         private void HandleReportAboveCP(long chatId, string[] args)
         {
             if (args.Length <= 0 | args.Length > 1)
+=======
+        private void HandleReportAboveCp(long chatId, string[] args)
+        {
+            if (args.Length <= 0 || args.Length > 1)
+>>>>>>> refs/remotes/Lunat1q/dev
             {
                 TlgrmBot.SendToTelegram("Error Invalid Command Structure!", chatId);
                 return;
             }
+<<<<<<< HEAD
             int CP;
             if (int.TryParse(args[0], out CP))
             {
                 if (CP < 0 | CP > 5000)
+=======
+            int cp;
+            if (int.TryParse(args[0], out cp))
+            {
+                if (cp < 0 || cp > 5000)
+>>>>>>> refs/remotes/Lunat1q/dev
                 {
                     TlgrmBot.SendToTelegram("Error Invalid CP Entered!", chatId);
                     return;
                 }
+<<<<<<< HEAD
                 TbReportAllPokemonsAboveCp.Text = CP.ToString();
                 SaveSettings();
                 TlgrmBot.SendToTelegram($"Set Report Above CP to: {CP}", chatId);
@@ -491,6 +515,15 @@ namespace Catchem.Pages
             }
             TlgrmBot.SendToTelegram("Error Invalid CP Entered!", chatId);
            }
+=======
+                TbReportAllPokemonsAboveCp.Text = cp.ToString();
+                SaveSettings();
+                TlgrmBot.SendToTelegram($"Set Report Above CP to: {cp}", chatId);
+                return;
+            }
+            TlgrmBot.SendToTelegram("Error Invalid CP Entered!", chatId);
+        }
+>>>>>>> refs/remotes/Lunat1q/dev
 
         private async void TelegramLogWorker()
         {
@@ -536,6 +569,7 @@ namespace Catchem.Pages
         public class TelegramBotOwner : CatchemNotified
         {
             private string _telegramName;
+
             public string TelegramName
             {
                 get { return _telegramName; }
@@ -547,6 +581,7 @@ namespace Catchem.Pages
             }
 
             private long _chatId;
+
             public long ChatId
             {
                 get { return _chatId; }
@@ -579,7 +614,8 @@ namespace Catchem.Pages
 
             private void HandleEvent(TelegramPokemonCaughtEvent eve)
             {
-                _receiver.PokemonCaught(eve.PokemonId, eve.Cp, eve.Iv, eve.ProfileName, eve.BotNicnname, eve.Level, eve.Move1, eve.Move2);
+                _receiver.PokemonCaught(eve.PokemonId, eve.Cp, eve.Iv, eve.ProfileName, eve.BotNicnname, eve.Level,
+                    eve.Move1, eve.Move2);
             }
 
             public void Listen(IEvent evt)
